bddc4fbbf700a95f667137ca4a94ac225b0f1838
20taxitaxi00 <takushi0210@icloud.com>
2020-11-14 14:12:58 +0900

    p

---
+++ guru/app.js
@@ -0,0 +1,21 @@
+function gnaviFreewordSearch(offset, hit_per_page){
+  var req = new XMLHttpRequest();
+  //keyid = github公開のため隠匿;
+  var freeword = document.forms.mainform.elements['freeword'].value
+  var pref = document.forms.mainform.elements['pref'].value
+  url = `https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=8b9899ee8b786d216eb0a28404ba7c4b&pref=${pref}&freeword=${freeword}&offset=${offset}&hit_per_page=${hit_per_page}`
+  
+  req.responseType = 'json'
+  
+  req.open('GET', url, true)
+
+  req.onload = function(){
+    for(i = 0; i < hit_per_page; i++){
+    APIで取得した店の名前と画像を表示
+    それぞれをぐるなびのサイトにリンクしている
+    document.getElementById("shops").childNodes[i].innerHTML 
+    = `<a href = ${req.response.rest[i].url}><img src = ${req.response.rest[i].image_url.shop_image1}><br>${req.response.rest[i].name}</a><br>`
+    }
+  };
+  req.send();
+};
